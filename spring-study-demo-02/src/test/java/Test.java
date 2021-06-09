d58e8aa18799b2b1cc04622540aae3a031b62258
HolunLi <Holun_Li@163.com>
2021-06-09 22:14:12 +0800

    '更新spring-study'

--- spring-study-demo-02/src/test/java/Test.java
+++ spring-study-demo-02/src/test/java/Test.java
@@ -1,11 +1,21 @@
 import com.holun.beans.Student;
+import com.holun.beans.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
         Student student = (Student) context.getBean("stu_1");
         System.out.println(student);
+    }*/
+
+    @org.junit.Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        User user1 = context.getBean("user_1", User.class);
+        User user2 = context.getBean("user_2", User.class);
+        System.out.println(user1);
+        System.out.println(user2);
     }
 }
